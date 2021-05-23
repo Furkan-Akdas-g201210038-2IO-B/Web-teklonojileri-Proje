e12f0444983fcbaaba5521bdae3e1728abb0a925
furkan akdaş <83430053+ako16180@users.noreply.github.com>
2021-05-23 05:51:19 +0300

    mirasımız sayfası oluşturuldu

---
+++ js/miras.js
@@ -0,0 +1,99 @@
+var bilgi=document.getElementsByClassName("bilgi");
+var a1=document.getElementById("a1");
+var a2=document.getElementById("a2");
+var a3=document.getElementById("a3");
+var a4=document.getElementById("a4");
+
+function funHakkinda()
+{
+    a1.style.color="blue";
+    a2.style.color="black";
+    a3.style.color="black";
+    a4.style.color="black";
+    
+    bilgi[0].innerHTML="Çifte Minareli Medrese Erzurum'un sembolü haline gelen " + 
+    "bir Selçuklu eseridir. Genelde 13'üncü yüzyılın sonlarında yaptırıldığı"+
+   "kabul edilmektedir. Selçuklu Sultanı Alaaddin Keykubat'ın kızı Hundi Hatun" +
+    "veya İlhanlı Hanedanları'ndan Padişah Hatun tarafından yaptırılmış olabileceği" +
+    "düşüncesi ile adına Hatuniye Medresesi de denilmektedir."+
+    "Yaklaşık 35x46 metre boyutlarında olan medrese iki katlı, dört eyvanlı ve açık avlulu"+
+     "medreseler grubunun en önemli örneğidir. 26x10 metre ölçülerindeki avlusu, dört yönden"+
+     "revaklarla çevrilidir. Avlunun her iki tarafında öğrenci ve öğretmen odaları sıralanmaktadır." +             
+   "Medresenin zemin katında on dokuz, birinci katında ise on sekiz odası bulunmaktadır."+
+     "Girişin batısındaki kare mekân, mescid olarak kullanılmıştır. Güneyde ana eyvanla" +
+     "birleşen, altında mumyalığı bulunan kümbet, Anadolu'da çağdaşları arasındaki en büyük"+
+      "türbe olma özelliğine sahiptir. Gövdesi on iki köşeli olan kümbet dıştan külah, içten"+
+       "kubbe ile örtülüdür. Saçağı, süsleme şerit ve silmelerle bezenmiştir."   +   
+    "Çifte Minareli Medrese'nin özellikle taç kapısında bulunan bezemeler, Selçuklu taş"+
+     "süslemesindeki derinliğin ve estetik anlayışın muhteşem örneklerini oluşturmaktadır." +
+     "Bezemelerde ağırlıklı olarak bitkisel ögeler kullanılmıştır. En çok palmet ve rumi motifleri kullanılırken, her ikisinin de birbiri ile uyumu dikkat çekmektedir."  +                                
+   "Taç kapının batı tarafına Orta Asya Türkleri'nin simgesi olan çift başlı kartal,"+
+     "ağzı açık iki yılan ve dilimli yapraktan oluşan hayat ağacı işlenmiştir."+
+    "Doğu tarafında bulunan simetriğinde ise yaprak ve kartal işlemesi yer almamaktadır."+
+    " Taç kapının iki yanında yükselen sırlı tuğla ve tuğla ile örülü, motiflerle bezeli minarelerin boyu 26 metredir."     +                          
+    "Minarelerde turkuaz rengiyle dikkat çeken panonun içinde Arapça 'Allah' yazmaktadır."+
+     "Harap halde iken Osmanlı padişahlarından 4. Murad tarafından tamir ettirilen ve bir süre 'tophane'" +
+     "olarak kullanılan medrese, 1942-1967 yılları arasında Erzurum Müzesi, günümüzde ise hem müze hem de resim sergi salonu olarak hizmet vermektedir.";
+}
+
+function funTarih()
+{
+    a1.style.color="black";
+    a2.style.color="blue";
+    a3.style.color="black";
+    a4.style.color="black";
+    
+    bilgi[0].innerHTML="Anadolu Selçuklu Sultanı I. Alaeddin Keykubad'ın kızı Hüdâvent Hatun "+
+ "tarafından 1253 yılında [1] yaptırılmış olan bu tarihi yapı, Anadolu'nun en büyük"+
+    "sanat şaheserlerinden biridir. Hüdâvent Hatun'dan dolayı 'Hatuniye Medresesi' olarak"+
+      "da adlandırılır."+
+      "Yaklaşık sekiz asır önce imar edilen bu şaheserin önceki dönemlerde Osmanlı" +
+      "Sultanı IV.Murat tarafından kapsamlı olarak tamir ettirilmiştir[2]. Bu tarihi "+
+      "eser gerek bölgede sık sık yaşanan depremler ve gerekse diğer olumsuz tabiat şartlarından "+
+      "kısmen de olsa olumsuz olarak etkilenmektedir. Son dönemde kısmi zemin kayması ve yüzey"+
+      " aşınmaları ile ilgili olarak; 2011 yılında devlet katkılarıyla başlatılan kapsamlı"+
+       " restorasyon çalışmaları, 2015 yılı itibarıyla devam etmektedir";
+}
+
+function funMimari()
+{
+    a1.style.color="black";
+    a2.style.color="black";
+    a3.style.color="blue";
+    a4.style.color="black";
+    
+    bilgi[0].innerHTML="Kümbeti Erzurum' da bulunan kümbetlerin en büyüğüdür"+
+    ".Her biri 26 metre yüksekliğindeki rengarenk çinilerle süslü çift minare, "+
+    "bu tarihi esere isim olmuştur. Avlulu, 2 katlı, 4 eyvanlı olup, 37 oda ve "+
+   "bir camiye sahiptir.[1]. 1.824 m² (38m x 48 m) lik bir alan üzerine kuruludur."+
+     "Açık avlulu medreselerin Anadolu 'daki en büyük örneğidir. Kuzey cephedeki taçkapı"+
+      "tam bir sanat eseridir. Taçkapı formundan ziyade çeşme nişleri ile yarım yuvarlak iki"+
+      " payanda vardır. Günümüzde kısmen tahrip olmuş görünümlü, 16 oluklu, firuze renkli çini" +
+       "kakmalı tuğladan yapma minarelerin kürsüleri de dikkat çekicidir. Taçkapının iki yanından" +
+      " yükselen silindirik minareler, tuğla ve mozaik çiniler ile süslenmiştir. Çinilerle süslü"+
+       " minarelere 'Allah', 'Muhammed' ve 'ilk dört büyük halife' nin isimleri de işlenmiştir."+
+         "Taç kapıyı çeviren bitki süslemeleri, kalın silmeli panoların içindeki 'ejder', 'hayatağacı'"+
+          "'kartal' motifleri cephenin en gösterişli bölümüdür. Taç kapının sağında ve solunda iki yönlü"+
+           "olmak üzere dört adet kabartma bulunmaktadır. Sağdakinde çift başlı kartal panosu yer"+
+            "almaktadır. Çifte Minareli Medrese mimarisinin ilk öne çıkan unsuru özelliğindeki geometrik"+
+             "süslemeler; en fazla avludaki sütun gövdelerinde, öğrenci odalarının kapı silmelerinde,"+
+              "eyvanların ön cephelerinde yer alır.";
+}
+function funKonum()
+{
+    a1.style.color="black";
+    a2.style.color="black";
+    a3.style.color="black";
+    a4.style.color="blue";
+    
+    bilgi[0].innerHTML="Erzurum şehir merkezinde ; Erzurum Ulu Camii bitişiğindeki"+
+     "alanda, Erzurum Kalesi ve Saat Kulesi ile karşı karşıya bir konumda yer almaktadır."+
+    " Medresenin ve iç kısımda bulunan kümbetin giriş kapıları başta olmak üzere;"+
+     " medrese mimarisinde yer alan önemli ve değerli parçalar, Rusların Erzurum’u "+
+     " işgali dönemlerinde Ruslar tarafından yerlerinden sökülerek Rusya'ya götürülmüştür." +
+     " [1]. Özellikle Medresenin kümbet üst kat giriş kapısı yan duvarlarındaki tahribat," +
+      " esere ne ölçüde zarar verildiğinin göstergesidir. Ayrıca Kümbet üst kat kısmında " +
+       "( bu kısımda dönemin müderrislerine ait mihrab görünümünde her birisi için köşeler "+
+        "de mevcuttur) tavandan aşağı sarkıtılan oldukça büyük ve uzun birbirine geçmeli sert"+
+         "mermerden yapma zincir de sökülmüştür.;"
+}
